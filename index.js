6217508940ffbfc22fa65008b0114e58a1acd9a4
Lyoung0789 <Lyoung070789@gmail.com>
2020-05-24 13:50:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+
+function superbowlWin(array){
+
+  let record = array.find(a => a.result === "W")
+  if (record){
+    return record.year
+  } else {
+    return undefined
+  }
+  
 }
